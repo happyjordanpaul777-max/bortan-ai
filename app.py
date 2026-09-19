a90eddcb0038b2514313704d0df0036055f25bea
happyjordanpaul777-max <happyjordanpaul777@gmail.com>
2026-09-19 17:10:59 +0530

    Fix merge conflict markers

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import os
 import time
@@ -140,15 +139,3 @@ if __name__ == "__main__":
         port=5000,
         debug=True
     )
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 0a01ac2728dae95bfe3fe343cb1da30dd95cac1b
